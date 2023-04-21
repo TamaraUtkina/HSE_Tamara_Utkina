3a17a1ef2736f319ede64ac24aa1a8a93cb260c8
TamaraUtkina <117661018+TamaraUtkina@users.noreply.github.com>
2023-04-21 19:30:31 +0300

    Update homework3.py
    Initial commit

--- lesson_2/homework3.py
+++ lesson_2/homework3.py
@@ -22,3 +22,41 @@ def get_maximum(numbers):
 # подпункт 3№
 def calc_area(side1, side2):
     return side1 * side2 / 2
+# импортируем переменные repondents и courts из файла lesson_2_data.py
+from lesson_2_data import repondents, courts
+
+# Функция для генерации "шапки"
+# Добавлен дополнительный параметр courts - в него при вызове передается список судов (для заполнения названия и адреса суда по коду из номера дела)
+def generate_header(respondent, courts):
+
+    court_code = respondent["case_number"].split("-")[0]
+
+    for court in courts:
+        if court["court_code"] == court_code:
+            court_name = court["court_name"]
+            court_address = court["court_address"]
+    
+    text = f'''
+В {court_name}
+Адрес: {court_address}
+    
+Истец: Пупкин Василий Геннадьевич
+ИНН 1236182357 ОГРНИП 218431927812733
+Адрес: 123534, г. Москва, ул. Водников, 13
+    
+Ответчик: {respondent["short_name"]}
+ИНН {respondent["inn"]} ОГРН {respondent["ogrn"]}
+Адрес: {respondent["address"]}
+    
+Номер дела {respondent["case_number"]}
+'''
+    return text
+
+
+# Функция для генерации "шапок" из списка ответчиков
+# respondents - список словарей с данными ответчиков
+# courts - список словарей с данными судов
+def all_headers(respondents, courts):
+    for respondent in respondents:
+        if "case_number" in respondent:
+            print(generate_header(respondent, courts))
