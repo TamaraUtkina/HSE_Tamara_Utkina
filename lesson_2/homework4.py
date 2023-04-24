e6b23ebb1355cabc6d939d660df74f411fe00dcc
TamaraUtkina <117661018+TamaraUtkina@users.noreply.github.com>
2023-04-24 19:16:35 +0300

    initial commit

--- lesson_2/homework4.py
+++ lesson_2/homework4.py
@@ -23,40 +23,46 @@ result = valid_org_inn("7812014560")
 
 ### Проверка ИНН для физического лица и ИП ###
 
-def valid_org_inn(inn):
-    numbers_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    numbers_2 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+my_inn = '770804312540'
 
-sum_1 = 0
-
- for i in range(numbers)):
-    sum_1 += numbers_1[i] * numbers_2[i]
-
-    control = sum_1 % 11
+def valid_person_inn(inn: str):
+    numbers_1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    numbers_2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+  
+  sum_1 = 0
+  sum_2 = 0
+  
+    for i, c in enumerate(inn):
+     if i > 9:
+        break
+    sum_1 +=(numbers_1[i] * (int(c)
+    sum_1 = sum_1 % 11
+    if sum_1 > 9:
+         sum_1 = sum_1 % 10
+    for i, c in enumerate(inn):
+      if sum_1 > 10:
+         break
+     sum_2 += (int(c) * k_2[i])
+    sum_2 = sum_2 % 11
+    if sum_2 > 9:
+        sum_2 = sum_2 % 10
+    pass
     
-    if control > 9:
-        control = control % 10
-    
-    if control == (inn[-1]):
-        return True
+  def is_valid_inn(inn: str):
+    if inn.isdigit() and len(inn) == 12:
+        return is_person_inn(inn)
+    elif inn.isdigit() and len(inn) == 10:
+        return is_org_inn(inn)
     else:
-        return False
+        raise TypeError
 
-def valid_org_inn(inn):
-    numbers_3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    numbers_4 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
 
-sum_2 = 0
+def task_3():
 
-for i in range(len(numbers)):
-    sum_2 += numbers_3[i] * numbers_4[i]
+    print(is_valid_inn(my_inn))
 
- if control > 9:
-        control = control % 11
-    
-    if sum_1 == (inn[-1]):
-    if sum_2 == (inn[-2]):
-     
-      return True
-    else:
-        return False
+
+def main():
+    # task_1()
+    # task_2()
+    task_3()
